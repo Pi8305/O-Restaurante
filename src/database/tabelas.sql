104ce7afb2b0cdc7f36ccef884cde74e8f517865
Pietro <pietro.dellarno@sptech.school>
2026-05-05 18:50:41 -0300

    tabelas e modelo

--- src/database/tabelas.sql
+++ src/database/tabelas.sql
@@ -1,8 +1,8 @@
 create database ORestaurante;
-use table ORestaurante;
+use ORestaurante;
 
 create table links (
-    idLinks int PRIMARY KEY AUTO_INCREMENT,
+    idLinks int PRIMARY KEY auto_increment,
     linktree varchar(150),
     instagram varchar(150),
     twitter varchar(150),
@@ -11,25 +11,39 @@ create table links (
     discord varchar(150),
     outros1 varchar(150),
     outros2 varchar(150)
-)
+);
 
 create table usuario (
-    idUsuario int PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45) not null,
-    email VARCHAR(45) not null,
-    senha VARCHAR(45) not null,
-    dtCriacao DATETIME not null,
+    idUsuario int PRIMARY KEY auto_increment,
+    nome varchar(45) not null,
+    email varchar(45) not null,
+    senha varchar(45) not null,
+    dtCriacao datetime default current_timestamp not null,
     descricao varchar(200),
     idade int,
     pronomes varchar(45),
     fkLinks int unique,
-    constraint fkLinksCOns foreign key (fkLinks) references links(idLinks)
+    constraint fkLinksCons foreign key (fkLinks) references links(idLinks)
 );
 
-create table autor (
-    idPost int AUTO_INCREMENT,
+create table post (
+	idPost int primary key auto_increment,
     fkUsuario int,
     nome varchar(45) not null,
     descricao varchar(200),
-    constraint PKComposta PRIMARY KEY 
+    likes int
 );
+
+create table tags (
+	idTag int primary key,
+    tag varchar(15)
+);
+
+create table conexao (
+	fkPost int,
+    fkTags int,
+    constraint PKComposta2 primary key (fkPost, fkTags)
+);
+
+insert into usuario (nome, email, senha) values 
+('aaaa', 'aaaa@bbbb', 'bbbb');
