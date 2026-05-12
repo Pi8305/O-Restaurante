75b6aebd8c6b89512df4a359477eb34ee59b8418
Pietro <pietro.dellarno@sptech.school>
2026-05-12 16:27:33 -0300

    Add files via upload

---
+++ src/controllers/postsController.js
@@ -0,0 +1,19 @@
+var usuarioModel = require("../models/postsModel");
+
+function renderizar() {
+    postsModel.renderizar()
+        .then(
+            res.json({
+                        id: resultadoAutenticar[0].id,
+                        email: resultadoAutenticar[0].email,
+                        nome: resultadoAutenticar[0].nome,
+                        senha: resultadoAutenticar[0].senha,
+                    })
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
